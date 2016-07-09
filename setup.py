88fde778e717284a5f492f6bdfeb978f006abb9d
Jake McGinty <me@jake.su>
2016-07-10 04:25:45 +0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='opie',
-    version='0.0.2',
+    version='0.0.3',
     author='Jake McGinty',
     author_email='me@jake.su',
     description=("A helping friend for the OP-1 synthesizer."),
