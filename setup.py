90b20ecf43003813d6d46e2e450c45af58d36f36
Jake McGinty <me@jake.su>
2017-04-27 17:15:26 +0800

    v0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='opie',
-    version='0.0.7',
+    version='0.0.8',
     author='Jake McGinty',
     author_email='me@jake.su',
     description=("A helping friend for the OP-1 synthesizer."),
